302e4899ce7d44b8f04896d402ae940fa39f0de6
chilagranate <31170204+chilagranate@users.noreply.github.com>
2018-06-12 23:37:33 -0300

    Agregada activity acerca de

--- app/src/main/java/com/chila/mascotas/Config.java
+++ app/src/main/java/com/chila/mascotas/Config.java
@@ -1,6 +1,6 @@
 package com.chila.mascotas;
 
 public class Config {
-    public static final String EMAIL ="chila.granate@gmail.com";
-    public static final String PASSWORD ="2603631as";
+    public static final String EMAIL ="";
+    public static final String PASSWORD ="";
 }
